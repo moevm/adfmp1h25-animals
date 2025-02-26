6bb1ddd6581a9e7f8c2199f83363b11500ed43b7
Olesya-Khulap <khulap.olesya@gmail.com>
2025-02-26 20:01:50 +0000

    added login screen

---
+++ app/src/main/java/com/example/animals/LoginScreen.kt
@@ -0,0 +1,85 @@
+package com.example.animals
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.*
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.animals.ui.theme.*
+
+@Composable
+fun LogInScreen(onLoginSuccess: () -> Unit, onBack: () -> Unit) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var isPasswordVisible by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(DarkBeige),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Вход",
+            style = ButtonsExtraBold,
+            fontWeight = FontWeight.ExtraBold,
+            fontSize = 40.sp,
+            color = Brown,
+            modifier = Modifier.padding(bottom = 99.dp)
+        )
+
+        LoginInputField(label = "Email:", value = email, onValueChange = { email = it })
+        Spacer(modifier = Modifier.height(16.dp))
+
+        LoginPasswordField(
+            label = "Пароль:",
+            password = password,
+            onPasswordChange = { password = it },
+            isPasswordVisible = isPasswordVisible,
+            onToggleVisibility = { isPasswordVisible = !isPasswordVisible }
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Button(
+            onClick = { onLoginSuccess() },
+            modifier = Modifier
+                .padding(17.dp)
+                .fillMaxWidth(1f),
+            shape = RoundedCornerShape(10.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Brown)
+        ) {
+            Text(
+                text = "Войти",
+                style = ButtonsExtraBold,
+                fontWeight = FontWeight.ExtraBold,
+                color = LightBeige,
+                modifier = Modifier.padding(6.dp)
+            )
+        }
+
+        TextButton(onClick = onBack) {
+            Text(
+                text = "Назад",
+                style = ButtonsExtraBold,
+                fontWeight = FontWeight.ExtraBold,
+                color = Brown,
+                modifier = Modifier.padding(6.dp)
+            )
+        }
+    }
+}
+
+
