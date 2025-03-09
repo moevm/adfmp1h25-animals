e90c9ecc7c2bb5ea4595af9a64084aefd1f588bd
Olesya-Khulap <khulap.olesya@gmail.com>
2025-03-09 14:23:35 +0000

    fix dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,7 +72,7 @@ dependencies {
     implementation(libs.volley)
     implementation(libs.androidx.preference.ktx)
     implementation(libs.firebase.auth.ktx)
-    implementation(libs.firebase.firestore.ktx)
+//    implementation(libs.firebase.firestore.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
